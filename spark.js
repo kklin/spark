c303de2b79035053ee24c3da8d517154444efa2e
Kevin Lin <kevin@quilt.io>
2017-07-26 09:47:34 -0700

    spark: Lint spark.js and sparkPI.js

--- spark.js
+++ spark.js
@@ -1,56 +1,70 @@
-const {Container, Service, publicInternet, LabelRule} = require("@quilt/quilt");
-var image = "quilt/spark";
+const { Container, Service, publicInternet, LabelRule } = require('@quilt/quilt');
 
+let image = 'quilt/spark';
+
+/**
+ * Change the Spark Docker image used to run the cluster.
+ *
+ * @param {string} newImage The Docker image used to run the cluster.
+ */
 function setImage(newImage) {
-    image = newImage;
+  image = newImage;
 }
 
+/**
+ * Spark represents a Spark cluster (a set of connected Spark masters and
+ * workers).
+ *
+ * @param {number} nMaster The number of masters to boot.
+ * @param {number} nWorker The number of workers to boot.
+ * @param {Service} [zookeeper] The Zookeeper service used to coordinate the
+ * Spark masters.
+ */
 function Spark(nMaster, nWorker, zookeeper) {
-    var dkms = new Container(image, ["run", "master"]).replicate(nMaster);
+  const dkms = new Container(image, ['run', 'master']).replicate(nMaster);
 
-    if (zookeeper) {
-        var zooHosts = zookeeper.children().join(",");
-        for (var i = 0; i < nMaster; i++) {
-            dkms[i].setEnv("ZOO", zooHosts);
-        }
-    }
+  if (zookeeper) {
+    const zooHosts = zookeeper.children().join(',');
+    dkms.forEach((master) => {
+      master.setEnv('ZOO', zooHosts);
+    });
+  }
 
-    this.masters = new Service("spark-ms", dkms);
+  this.masters = new Service('spark-ms', dkms);
 
-    var dkws = new Container(image, ["run", "worker"])
-        .withEnv({"MASTERS": this.masters.children().join(",")})
-        .replicate(nWorker);
-    this.workers = new Service("spark-wk", dkws);
+  const dkws = new Container(image, ['run', 'worker'])
+    .withEnv({ MASTERS: this.masters.children().join(',') })
+    .replicate(nWorker);
+  this.workers = new Service('spark-wk', dkws);
 
-    this.workers.connect(7077, this.workers);
-    this.workers.connect(7077, this.masters);
-    if (zookeeper) {
-        this.masters.connect(2181, zookeeper);
-    }
+  this.workers.connect(7077, this.workers);
+  this.workers.connect(7077, this.masters);
+  if (zookeeper) {
+    this.masters.connect(2181, zookeeper);
+  }
 
-    this.job = function(command) {
-        var cnt = this.masters.containers;
-        for (var i = 0; i < cnt.length; i++) {
-            cnt[i].env["JOB"] = command;
-        }
-        return this;
-    }
+  this.job = function job(command) {
+    this.masters.containers.forEach((master) => {
+      master.setEnv('JOB', command);
+    });
+    return this;
+  };
 
-    this.exposeUIToPublic = function() {
-        publicInternet.connect(8080, this.masters);
-        publicInternet.connect(8081, this.workers);
-        return this;
-    }
+  this.exposeUIToPublic = function exposeUIToPublic() {
+    publicInternet.connect(8080, this.masters);
+    publicInternet.connect(8081, this.workers);
+    return this;
+  };
 
-    this.exclusive = function() {
-        this.masters.place(new LabelRule(true, this.workers));
-        return this;
-    }
+  this.exclusive = function exclusive() {
+    this.masters.place(new LabelRule(true, this.workers));
+    return this;
+  };
 
-    this.deploy = function(deployment) {
-        deployment.deploy(this.masters);
-        deployment.deploy(this.workers);
-    }
+  this.deploy = function deploy(deployment) {
+    deployment.deploy(this.masters);
+    deployment.deploy(this.workers);
+  };
 }
 
 exports.setImage = setImage;
