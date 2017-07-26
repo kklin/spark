86eb8130067fbd46426c273d42bf3cc21159d4ce
Kevin Lin <kevin@quilt.io>
2017-07-26 09:47:34 -0700

    spark: Lint spark.js and sparkPI.js

--- sparkPI.js
+++ sparkPI.js
@@ -1,31 +1,37 @@
-const {createDeployment, Machine, publicInternet, githubKeys, enough} = require("@quilt/quilt");
-var spark = require("./spark.js");
+const {
+  Machine,
+  createDeployment,
+  enough,
+  githubKeys,
+  publicInternet,
+} = require('@quilt/quilt');
+const spark = require('./spark.js');
 
-var deployment = createDeployment({});
+const deployment = createDeployment({});
 
 // We will have three worker machines.
-var nWorker = 3;
+const nWorker = 3;
 
 // Application
 // sprk.exclusive enforces that no two Spark containers should be on the same
 // node. sprk.exposeUIToPublic says that the the public internet should be able
 // to connect to the Spark web interface. sprk.job causes Spark to run that
 // job when it boots.
-var sprk = new spark.Spark(1, nWorker)
-    .exclusive()
-    .exposeUIToPublic()
-    .job("run-example SparkPi");
+const sprk = new spark.Spark(1, nWorker)
+  .exclusive()
+  .exposeUIToPublic()
+  .job('run-example SparkPi');
 
 // Infrastructure
-var baseMachine = new Machine({
-    provider: "Amazon",
-    region: "us-west-1",
-    size: "m4.large",
-    diskSize: 32,
-    sshKeys: githubKeys("ejj"), // Replace with your GitHub username.
+const baseMachine = new Machine({
+  provider: 'Amazon',
+  region: 'us-west-1',
+  size: 'm4.large',
+  diskSize: 32,
+  sshKeys: githubKeys('ejj'), // Replace with your GitHub username.
 });
-deployment.deploy(baseMachine.asMaster())
-deployment.deploy(baseMachine.asWorker().replicate(nWorker + 1))
+deployment.deploy(baseMachine.asMaster());
+deployment.deploy(baseMachine.asWorker().replicate(nWorker + 1));
 deployment.deploy(sprk);
 
 deployment.assert(publicInternet.canReach(sprk.masters), true);
