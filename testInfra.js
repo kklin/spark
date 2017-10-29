a4296eac434893e3ff4819bb84d755da0d8dcf66
Luise Valentin <luise@kelda.io>
2017-10-28 22:41:19 -0500

    testInfra: Rename `quilt` to `kelda`

--- testInfra.js
+++ testInfra.js
@@ -1,7 +1,7 @@
 // This file contains the base infrastructure used for the travis build.
-function infraGetter(quilt) {
-  const inf = quilt.createDeployment({});
-  const vmTemplate = new quilt.Machine({ provider: 'Amazon' });
+function infraGetter(kelda) {
+  const inf = kelda.createDeployment({});
+  const vmTemplate = new kelda.Machine({ provider: 'Amazon' });
   inf.deploy(vmTemplate.asMaster().replicate(1));
   inf.deploy(vmTemplate.asWorker().replicate(3));
   return inf;
