84d8a319ac5580ab93971b0d2064cc93b5830c9a
Luise Valentin <luise@kelda.io>
2017-10-28 22:45:41 -0500

    testInfra: Use Infrastructure constructor
    This commit replaces the deprecated `createDeployment` function with the
    new `Infrastructure` constructor.

--- testInfra.js
+++ testInfra.js
@@ -1,10 +1,7 @@
 // This file contains the base infrastructure used for the travis build.
 function infraGetter(kelda) {
-  const inf = kelda.createDeployment({});
   const vmTemplate = new kelda.Machine({ provider: 'Amazon' });
-  inf.deploy(vmTemplate.asMaster().replicate(1));
-  inf.deploy(vmTemplate.asWorker().replicate(3));
-  return inf;
+  return new kelda.Infrastructure(vmTemplate, vmTemplate.replicate(3));
 }
 
 module.exports = infraGetter;
