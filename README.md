1889f755ef53cb3339691f57603e57a65bd89def
Kay Ousterhout <kay@kelda.io>
2017-11-03 16:23:56 -0700

    blueprint: Run driver in its own container
    The driver is its own process, so this commit runs the driver
    as a separate container. Ideally the driver and the master should
    be on the same machine.  That appears to happen by default with
    the current scheduler, but once Kelda placement policies support
    it, the blueprint should explicitly specify that these should be
    on the same machine.
    
    This commit also updates the readme to describe how to interact with
    the driver.

--- README.md
+++ README.md
@@ -42,51 +42,93 @@ Next, run the Spark blueprint:
 $ kelda run ./sparkRun.js
 ```
 
-This will use the blueprint `sparkRun.js` to launch a cluster of Spark workers
-and a Spark master. It will take a bit for the VMs to boot up, for Kelda to
+This will use the blueprint `sparkRun.js` to launch a Spark cluster. It will take a
+bit for the VMs to boot up, for Kelda to
 configure the network, and for Docker containers to be initialized. The command
 `kelda show` provides useful information about the VMs and containers in the
 deployment. The following output reports that the Master VM's public IP is
-`54.193.66.67`:
+`54.219.158.97`:
 
 ```
 $ kelda show
-MACHINE         ROLE      PROVIDER    REGION       SIZE        PUBLIC IP         CONNECTED
-12c000f87ccf    Master    Amazon      us-west-1    m4.large    54.193.66.67      true
-4316db8165d3    Worker    Amazon      us-west-1    m4.large    54.183.187.137    true
-...
+MACHINE         ROLE      PROVIDER    REGION       SIZE         PUBLIC IP        STATUS
+i-06a6afa426    Master    Amazon      us-west-1    m3.xlarge    54.219.158.97    connected
+i-0f3add6b27    Worker    Amazon      us-west-1    m3.xlarge    54.67.108.98     connected
+i-08f8c31070    Worker    Amazon      us-west-1    m3.xlarge    52.53.157.29     connected
+i-027e5d72ed    Worker    Amazon      us-west-1    m3.xlarge    54.183.210.94    connected
 ```
 
-When `CONNECTED` is `true`, the corresponding VM is fully booted and has begun
+When `STATUS` is `connected`, the corresponding VM is fully booted and has begun
 communicating with Kelda:
 
-Once a machine is `CONNECTED`, you can ssh into to the VM with the command
-`kelda ssh <MACHINE_ID>`, (for the machines above, `kelda ssh 12c`) or manually
-with `ssh kelda@<PUBLIC_IP>`.
+Once a machine is `connected`, you can ssh into to the VM with the command
+`kelda ssh <MACHINE_ID>`, (for the machines above, `kelda ssh i-06a` to ssh into
+the master).
 
 To list all active containers in the cluster, execute `kelda show` again, and
-look for `CONTAINER` at the end of the output.  For example:
+look for the `CONTAINER` section at the end of the output.  For example:
 
 ```
 $ kelda show
 ...
-CONTAINER       MACHINE         COMMAND                   LABELS      STATUS     CREATED           PUBLIC IP
-13d6b800086a    4316db8165d3    keldaio/spark run master  spark-ms    running    34 seconds ago    54.183.187.137:8080
-
-063f91f017b2    a7749c79b8fe    keldaio/spark run worker  spark-wk    running    34 seconds ago    54.241.148.64:8081
+CONTAINER       MACHINE         COMMAND HOSTNAME        STATUS     CREATED              PUBLIC IP
+0b8eb391d8ce    i-027e5d72ed    ...     spark-ms        running    About an hour ago    54.183.210.94:8080
+62567da92a76    i-08f8c31070    ...     spark-wk        running    About an hour ago    52.53.157.29:8081
+9f7eee19a330    i-0f3add6b27    ...     spark-driver    running    About an hour ago    54.67.108.98:[4040,18080]
+b8010a0970b3    i-0f3add6b27    ...     spark-wk2       running    About an hour ago    54.67.108.98:8081
 ...
 ```
 
-## Run an example job that computes Pi
+The blueprint launches a few containers.  The container that users will interact with
+is the container with hostname `spark-driver`, which is intended to be used to launch
+Spark jobs (the following section describes how to launch a job in more detail). The
+blueprint also creates a container with hostname `spark-ms` that runs the Spark
+Standalone Scheduler Master (which handles scheduling Spark applications), and many
+Spark worker containers, which run tasks for Spark jobs.
 
-To compute Pi, you can use the `run-example` command on the Spark master.
-First, determine the ID of the Spark master container (the master has the
-container label `spark-ms`).  In the example above, the container running
-the Spark master has ID 13d6b.  Next, use that ID to launch the SparkPi
-job:
+## Running Jobs
+
+Users can run Spark jobs using the `spark-driver` container.  This container is configured
+to automatically submit jobs to the standalone master to be scheduled, and the network
+for this container is configured to allow public access to the Spark UI.  To login to the
+Spark driver, find the container ID corresponding to the container with hostname
+`spark-driver`, and use the `kelda ssh` command.  For example, for the `kelda show` output
+above:
 
 ```console
-$ kelda ssh 13d6b run-example SparkPi
+$ kelda ssh 9f7ee
+```
+
+All of the Spark commands are included in the container path.  To launch the Spark shell,
+once SSH'ed into the container, run `spark-shell`:
+
+```console
+$ spark-shell
+Spark context Web UI available at http://54.67.108.98:4040
+Spark context available as 'sc' (master = spark://spark-ms.q:7077, app id = app-20171107203134-0001).
+Spark session available as 'spark'.
+Welcome to
+      ____              __
+     / __/__  ___ _____/ /__
+    _\ \/ _ \/ _ `/ __/  '_/
+   /___/ .__/\_,_/_/ /_/\_\   version 2.2.0
+      /_/
+
+Using Scala version 2.11.8 (OpenJDK 64-Bit Server VM, Java 1.8.0_131)
+Type in expressions to have them evaluated.
+Type :help for more information.
+
+scala>
+```
+
+### Run an example job that computes Pi
+
+To compute Pi, you can use the `run-example` command on the Spark driver.
+You can `ssh` into the container, as above, or you can run the job by passing a
+command into `kelda ssh`:
+
+```console
+$ kelda ssh 9f7ee run-example SparkPi
 ```
 
 This command will launch a Spark job, and the jobs will be printed to
--- spark.js
+++ spark.js
@@ -42,7 +42,9 @@ function getConfigFiles() {
 
 class Spark {
   /**
-   * Creates a Spark cluster (a master and a collection of workers).
+   * Creates a Spark cluster. The cluster will include a collection of worker containers,
+   * a master container (which handles scheduling), and a container that the user can use to
+   * run a Spark application.
    *
    * @param {number} nWorker The number of Spark worker containers to create.
    */
@@ -50,13 +52,11 @@ class Spark {
     const sparkConfigFiles = getConfigFiles();
 
     this.master = new Container('spark-ms', image, {
-      command: ['sh', '-c',
-        '/spark/sbin/start-history-server.sh && ' +
-        '/spark/bin/spark-class org.apache.spark.deploy.master.Master'],
+      command: ['/spark/bin/spark-class', 'org.apache.spark.deploy.master.Master'],
       filepathToContent: sparkConfigFiles,
     });
-    const masterURL = `spark://${this.master.getHostname()}:7077`;
-    this.master.setEnv('MASTER', masterURL);
+    this.masterPort = 7077;
+    this.masterURL = `spark://${this.master.getHostname()}:${this.masterPort}`;
 
     this.workers = [];
     for (let i = 0; i < nWorker; i += 1) {
@@ -64,31 +64,69 @@ class Spark {
         command: [
           '/spark/bin/spark-class',
           'org.apache.spark.deploy.worker.Worker',
-          masterURL,
+          this.masterURL,
         ],
         filepathToContent: sparkConfigFiles,
       }));
     }
 
     // Allow Spark workers to access the Spark Standalone Master.
-    allow(this.workers, this.master, 7077);
+    allow(this.workers, this.master, this.masterPort);
+    // XXX: This is only necessary so that spark nodes can ask an external
+    // service what their public IP is.  Once this information can be passed in
+    // through an environment variable, these ACLs should be removed.
+    publicInternet.allowFrom(this.workers, 80);
+    publicInternet.allowFrom(this.master, 80);
+
+    // Add a container to run the Spark Driver, which manages a particular
+    // application. This container should be used to launch user jobs.
+    // XXX: Currently this container will run on an arbitrary machine. It
+    // should ideally run on the same machine that the master is running on
+    // and not on a worker, where it may contend with the worker JVM.
+    this.addDriver();
+  }
+
+  /**
+   * Adds a container that users can use to run Spark jobs (e.g., by launching
+   * spark-shell).
+   *
+   * @returns {void}
+   */
+  addDriver() {
+    this.driver = new Container('spark-driver', image, {
+      // When we start the container, start the history server. The command to start the
+      // history server starts it as a daemon, so we need to add a second, long-running command
+      // to ensure that the container doesn't exit.
+      command: ['sh', '-c', '/spark/sbin/start-history-server.sh && tail -f /dev/null'],
+      filepathToContent: getConfigFiles(),
+      env: { MASTER: this.masterURL },
+    });
+
+    // Allow the driver to connect to the standalone master.
+    allow(this.driver, this.master, this.masterPort);
+
     // Allow Spark workers to access the Spark driver (this port is configured
     // in spark-defaults.conf).
-    allow(this.workers, this.master, 36666);
+    allow(this.workers, this.driver, 36666);
+
     // Allow Spark workers to access the Spark block manager, on both the
     // master and on all of the other Spark workers (this is used to fetch
     // task information from the master and to fetch data from other
     // workers). This port is configured in spark-defaults.conf.
     const sparkBlockManagerPort = 36667;
     allow(this.workers, this.workers, sparkBlockManagerPort);
-    allow(this.workers, this.master, sparkBlockManagerPort);
-    allow(this.master, this.workers, sparkBlockManagerPort);
+    allow(this.workers, this.driver, sparkBlockManagerPort);
+    allow(this.driver, this.workers, sparkBlockManagerPort);
 
-    // XXX: This is only necessary so that spark nodes can ask an external
-    // service what their public IP is.  Once this information can be passed in
-    // through an environment variable, these ACLs should be removed.
-    publicInternet.allowFrom(this.workers, 80);
-    publicInternet.allowFrom(this.master, 80);
+    // XXX: This is only necessary so that the driver can learn its public IP
+    // (see note above).
+    publicInternet.allowFrom(this.driver, 80);
+
+    const driverHostname = this.driver.hostname;
+    console.log(`Spark driver started with hostname "${driverHostname}". ` +
+      'To run a Spark job:\n(1) Use `kelda show` to find the ID of the container ' +
+      `with hostname "${driverHostname}"\n(2) Use \`kelda ssh\` to login to that ` +
+      'container\n(3) Run a job, e.g., by running `spark-submit` or `spark-shell`');
   }
 
   exposeUIToPublic() {
@@ -96,18 +134,22 @@ class Spark {
     // the applications that have run).
     allow(publicInternet, this.master, 8080);
     allow(publicInternet, this.workers, 8081);
-    allow(publicInternet, this.master, 18080);
+
+    // Enable access to the history server, which shows information about jobs that
+    // have run as part of applications that are now complete.
+    allow(publicInternet, this.driver, 18080);
 
     // Expose the per-job UI (which shows each application).
     // This will only work when there's only one job; otherwise, the UI will use
     // increasing port numbers for the other jobs, and those will not be accessible.
-    allow(publicInternet, this.master, 4040);
+    allow(publicInternet, this.driver, 4040);
 
     return this;
   }
 
   deploy(deployment) {
     this.master.deploy(deployment);
+    this.driver.deploy(deployment);
     this.workers.forEach(worker => worker.deploy(deployment));
   }
 }
