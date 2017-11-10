a6c521eec9a41d06a8325a1dd82e0ec571ef9dbc
Kay Ousterhout <kay@kelda.io>
2017-11-10 14:40:00 -0800

    blueprint: Configure Spark's memory
    By default, Spark will give each executor JVM 1GB of memory. This
    commit explicitly configures the executor memory, so that users can
    pass in the amount of memory they'd like to allocate to each Spark
    executor, and changes sparkRun.js to automatically set Spark's
    memory based on the machines being used.

--- spark.js
+++ spark.js
@@ -14,20 +14,28 @@ function setImage(newImage) {
 }
 
 /**
+ * @param {number} memoryGB - The amount of memory that each Spark executor should
+ *   be configured to use. See the Spark class documentation for more on how to
+ *   set this.
  * @returns {Object.<String,String>} Spark configuration files that should be added
  *   to all masters and workers in a Spark cluster.  The files are returned in the
  *   format expected by the Container filepathToContents argument (i.e., as a mapping
  *   of filenames to the contents of that file).
  */
-function getConfigFiles() {
+function getConfigFiles(memoryGB) {
   // Set a spark-env.sh file on each machine, which will be sourced before starting
   // any Spark processes.
   const sparkEnvContent = fs.readFileSync(path.join(__dirname, 'spark-env.sh'),
     { encoding: 'utf8' });
   // Add a spark-defaults.conf, which can be used to configure the default job
   // configuration.
-  const sparkConfContent = fs.readFileSync(path.join(__dirname, 'spark-defaults.conf'),
+  let sparkConfContent = fs.readFileSync(path.join(__dirname, 'spark-defaults.conf'),
     { encoding: 'utf8' });
+  // Add configuration for how much memory Spark should use. Convert the memory to MB,
+  // because Spark doesn't accept fractional values, and rounding to the nearest GB
+  // loses too much accuracy.
+  const memoryMB = Math.round(1024 * memoryGB);
+  sparkConfContent += `\nspark.executor.memory ${memoryMB}m`;
 
   // Add the log4j configuration, which manages the log format and verbosity.
   const log4jConfigContent = fs.readFileSync(path.join(__dirname, 'log4j.properties'),
@@ -47,9 +55,12 @@ class Spark {
    * run a Spark application.
    *
    * @param {number} nWorker The number of Spark worker containers to create.
+   * @param {number} [memoryGB=1] The amount of memory (in GB) that each Spark executor should be
+   *   given.  When Spark is running in isolation, this should be set to roughly 90%
+   *   of the memory on the worker machine (to leave room for the worker process, the OS, etc.).
    */
-  constructor(nWorker) {
-    const sparkConfigFiles = getConfigFiles();
+  constructor(nWorker, memoryGB = 1) {
+    const sparkConfigFiles = getConfigFiles(memoryGB);
 
     this.master = new Container('spark-master', image, {
       command: ['/spark/bin/spark-class', 'org.apache.spark.deploy.master.Master'],
@@ -83,22 +94,24 @@ class Spark {
     // XXX: Currently this container will run on an arbitrary machine. It
     // should ideally run on the same machine that the master is running on
     // and not on a worker, where it may contend with the worker JVM.
-    this.addDriver();
+    this.addDriver(sparkConfigFiles);
   }
 
   /**
    * Adds a container that users can use to run Spark jobs (e.g., by launching
    * spark-shell).
    *
+   * @param {Object.<string, string>} configFiles - Map of filename to contents of the
+   *   Spark configuration files that should be added to the driver.
    * @returns {void}
    */
-  addDriver() {
+  addDriver(configFiles) {
     this.driver = new Container('spark-driver', image, {
       // When we start the container, start the history server. The command to start the
       // history server starts it as a daemon, so we need to add a second, long-running command
       // to ensure that the container doesn't exit.
       command: ['sh', '-c', '/spark/sbin/start-history-server.sh && tail -f /dev/null'],
-      filepathToContent: getConfigFiles(),
+      filepathToContent: configFiles,
       env: { MASTER: this.masterURL },
     });
 
--- sparkRun.js
+++ sparkRun.js
@@ -4,8 +4,9 @@ const spark = require('./spark.js');
 const inf = kelda.baseInfrastructure();
 
 const workerVMs = inf.machines.filter(machine => machine.role === 'Worker');
+const workerMemoryGb = workerVMs[0].ram;
 
-const s = new spark.Spark(workerVMs.length - 1);
+const s = new spark.Spark(workerVMs.length - 1, workerMemoryGb * 0.9);
 s.exposeUIToPublic();
 
 s.deploy(inf);
