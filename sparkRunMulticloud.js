1325b3ea8fb368e95cda13fbb989e6827955ab04
Kevin Lin <kevin@kelda.io>
2018-03-27 15:13:32 -0700

    demo: Add demo blueprints for multiregion and single region

--- sparkRunMulticloud.js
+++ sparkRunMulticloud.js
@@ -1,14 +1,18 @@
 const kelda = require('kelda');
 const spark = require('./spark.js');
 
-const defaultWorker = new kelda.Machine({
+const defaultWorkerOpts = {
   provider: 'Amazon',
   ram: new kelda.Range(4),
   cpu: new kelda.Range(2),
-});
+};
 const inf = new kelda.Infrastructure({
   masters: new kelda.Machine({ provider: 'Amazon', size: 'm3.medium' }),
-  workers: defaultWorker.replicate(3),
+  workers: [
+    new kelda.Machine(defaultWorkerOpts),
+    new kelda.Machine(Object.assign(defaultWorkerOpts, { provider: 'Google' })),
+    new kelda.Machine(Object.assign(defaultWorkerOpts, { provider: 'DigitalOcean' })),
+  ],
 });
 
 const s = new spark.Spark(inf.workers.length - 1,
